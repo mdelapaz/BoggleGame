3e40dc90317d284bd80cb3fa99b68a9c24ad3ae8
Tristan de Nijs <tristan_de_nijs@yahoo.com>
2017-03-13 21:26:21 -0700

    Added client score so we can keep track of things, so we can use the client score and then return that to the other user at the end. Then, they can put that in their highscores.

--- app/src/main/java/com/example/miguel/bogglegame/AppLogic/BoggleBoard.java
+++ app/src/main/java/com/example/miguel/bogglegame/AppLogic/BoggleBoard.java
@@ -38,6 +38,7 @@ public class BoggleBoard {
     //The score of each valid word is counted based on its length, 1 point for 3 or 4 letter words, 2 points for 5 letter words, 3
     //points for 6 letter words, 5 points for 7 letter words, and 10 points for words of 8 or more letters.
     private int score;
+    private int clientScore;
     //validWordsFoundByUser contains all valid words on boggle board that are found by user
     public Set<String> validWordsFoundByUser = new HashSet<String>();
 
@@ -53,6 +54,7 @@ public class BoggleBoard {
     public BoggleBoard(final int boardLength, final String[] wordsInDictionary, int difficultyLevel, Context c, GameMode gameMode) {
 
         score = 0;
+        clientScore = 0;
         this.context = c;
         this.boardLength = boardLength;
         this.difficultyLevel = difficultyLevel;
@@ -102,6 +104,7 @@ public class BoggleBoard {
     //NOTE : use this constructor for MultiPlayer Mode where the player is NOT the game initiator
     public BoggleBoard(int boardLength, Context context, char[][] board, int difficultyLevel, GameMode gameMode, final String[] wordsInDictionary) {
         score = 0;
+        clientScore = 0;
         this.boardLength = boardLength;
         this.context = context;
         this.board = board;
@@ -313,6 +316,8 @@ public class BoggleBoard {
 
                     if(!checkForClient){
                         score += wordScore;
+                    }else{
+                        clientScore += wordScore;
                     }
 
                     return wordScore;
@@ -477,6 +482,14 @@ public class BoggleBoard {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public void setClientScore(int clientScore) {
+        this.clientScore = clientScore;
+    }
+
+    public int getClientScore() {
+        return this.clientScore;
     }
 
     public GameMode getGameMode() {
